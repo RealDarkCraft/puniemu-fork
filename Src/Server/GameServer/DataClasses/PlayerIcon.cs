5c68ae467b436e9629ab97d0becf5f78fb16b3cf
DarkCraft <71570182+RealDarkCraft@users.noreply.github.com>
2025-09-20 14:33:09 +0200

    Delete Src/Server/GameServer/DataClasses directory

--- Src/Server/GameServer/DataClasses/PlayerIcon.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Puniemu.Src.Server.GameServer.DataClasses
-{
-    public enum PlayerIcon
-    {
-        Boy = 1,
-        Girl = 2
-    }
-}
--- Src/Server/GameServer/DataClasses/PlayerTitle.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Puniemu.Src.Server.GameServer.DataClasses
-{
-    public enum PlayerTitle
-    {
-        Kun = 1,
-        Chan = 2
-    }
-}
